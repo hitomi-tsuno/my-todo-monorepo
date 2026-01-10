8852589476ca93dd646f6608afafaeab0562c2f5
hitomi-tsuno <h.tsuno.1234@docomo.ne.jp>
2026-01-10 13:34:55 +0900

    デバッグ環境の構築

--- backend/api/db.php
+++ backend/api/db.php
@@ -3,9 +3,11 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 function getDB()
 {
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-    $dotenv->load(); // .envファイルから環境変数をロード
-    $db = new PDO('sqlite:' . __DIR__ . $_ENV['DB_PATH']);
+    // $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+    // $dotenv->load(); // .envファイルから環境変数をロード
+    // $dbPath = __DIR__ . '/../' . getenv('DB_PATH');
+    $dbPath = __DIR__ . '/../data/todos.db';
+    $db = new PDO('sqlite:' . $dbPath);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $db;
 }
